412561b76167cd200af4d61ec8343b30b9c09cd9
ayrton <ayrtonhe@live.com>
2021-01-05 19:35:56 -0300

    iniciando projeto

---
+++ app/src/main/java/com/fiap/fiap_android_seguros/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fiap.fiap_android_seguros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
